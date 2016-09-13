ff97eb84b6249c799759321b7ab736d9675fd322
v.petrov <mailto.vpv@gmail.com>
2016-09-13 10:49:08 +0300

    initial commit

---
+++ lab1-4.asm
@@ -0,0 +1,50 @@
+org 00h
+	jmp start
+
+org 100h
+
+start:	mov 	dptr, #array
+	mov 	r0, #0bh
+	mov 	r1, #0h
+
+; Calculating average of array
+avrl1:  mov 	a, r0
+	movc 	a, @a+dptr
+
+	add	a, r1
+	mov	r1, a
+
+	dec 	r0
+	cjne 	r0, #01h, avrl1
+
+	mov	a, r1
+	mov	b, #0ah
+	div	ab
+	mov	r1, a ; R1 contains average of array[2..11]
+
+	clr	p0.0
+	clr	p0.1
+
+; Checking if average is > than Qmax
+	mov	a, #0h
+	movc	a, @a+dptr
+	subb	a, r1
+	jnc	tmin
+
+	setb	p0.0
+	setb	p0.1;
+	jmp	quit
+
+; Checkng if average is > than Qmin
+tmin:	mov	a, #01h
+	movc	a, @a+dptr
+	subb	a, r1
+	jnc	quit
+	setb	p0.1
+
+quit:	jmp $
+
+org 1000h
+	array: db 7,5,1,2,3,4,5,6,7,8,9,10 ; max, min, data array -> size 10
+
+end
